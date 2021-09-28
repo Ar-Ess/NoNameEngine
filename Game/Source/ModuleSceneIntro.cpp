d6620f73b6ddcce6bac07cde862e5f27847e7994
BooStarGamer <marti.buxeda@gmail.com>
2021-09-28 09:34:49 +0200

    Almost

---
+++ Game/Source/ModuleSceneIntro.cpp
@@ -0,0 +1,36 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleRender.h"
+#include "ModuleSceneIntro.h"
+#include "ModuleInput.h"
+#include "ModuleTextures.h"
+
+ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+ModuleSceneIntro::~ModuleSceneIntro()
+{}
+
+bool ModuleSceneIntro::Start()
+{
+	LOG("Loading Intro assets");
+	bool ret = true;
+
+	App->renderer->camera.x = App->renderer->camera.y = 0;
+
+	return ret;
+}
+
+bool ModuleSceneIntro::CleanUp()
+{
+	LOG("Unloading Intro scene");
+
+	return true;
+}
+
+update_status ModuleSceneIntro::Update()
+{
+
+	return UPDATE_CONTINUE;
+}
