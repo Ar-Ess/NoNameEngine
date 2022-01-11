804bf28746f6dc5a854ff86acf0db14ac1736424
BooStarGamer <marti.buxeda@gmail.com>
2022-01-11 14:35:51 +0100

    NNE - Audio Source Window update
    Step:
     - Create Audio Source Component
    
    Commit:
     - New knobs & information

--- Game/Source/AudioSourceComponent.h
+++ Game/Source/AudioSourceComponent.h
@@ -2,6 +2,8 @@
 #define __AUDIO_SOURCE_COMPONENT_H__
 
 #include "Component.h"
+#include "External/Wwise/include/AK/IBytes.h"
+#include "External/Wwise/include/AK/SoundEngine/Common/AkSoundEngine.h"
 
 class AudioSourceComponent : public Component
 {
@@ -25,10 +27,30 @@ private: // Methods
 		{
 			if (onWindow != nullptr && !*onWindow) *onWindow = ImGui::IsWindowHovered();
 
+			ImGui::Text("Name of the sample");
+			ImGui::Text("kHz, Bit, Channels info");
+
+			ImGui::Dummy(ImVec2{ 0.0f, 3.8f });
+
 			ImGui::Dummy(ImVec2{ 3.8f, 0.0f }); ImGui::SameLine();
-			ImGui::VSliderInt("- 0", ImVec2{15, 150}, &volume, -128.0f, 0.0f, "");
-			ImGui::Dummy(ImVec2{ 0.0f, 3.0f });
-			ImGui::Knob("L / R", &pan, -50.0, 50.0, false);
+			ImGui::VSliderInt("-0", ImVec2{15, 150}, &volume, -128.0f, 0.0f, ""); 
+			ImGui::SameLine(0.0f, 20.0f);
+			if (ImGui::BeginTable("Column Table", 2))
+			{
+				ImGui::TableNextRow(); ImGui::TableSetColumnIndex(0);
+				ImGui::Dummy(ImVec2{0.0f, 6.0f});
+
+				ImGui::TableNextRow(); ImGui::TableSetColumnIndex(0);
+				ImGui::Knob("L / R", &pan, -50.0, 50.0, false);
+				ImGui::Dummy(ImVec2{ 0.0f, 10.0f });
+
+				ImGui::TableNextRow(); ImGui::TableSetColumnIndex(0);
+				ImGui::Knob("Transpose", &transpose, -24.0, 24.0, false);
+				ImGui::Dummy(ImVec2{ 0.0f, 10.0f });
+				
+			}
+			ImGui::EndTable();
+
 		}
 		ImGui::End();
 	}
@@ -37,6 +59,7 @@ private: // Variables
 
 	int volume = 0;
 	float pan = 0;
+	float transpose = 0;
 };
 
 #endif // !__AUDIO_SOURCE_COMPONENT_H__
